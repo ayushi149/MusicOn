bdf30e6c57763e96d5ab86c60557463b1c6f3fd1
Ayushi Devliya <111689547+ayushi149@users.noreply.github.com>
2023-10-31 20:33:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,106 @@
+    console.log("welcome to spotify");
+
+//Initialize the variables
+let songIndex=0;
+let audioElement = new Audio('song_files/1.mp3');
+let masterPlay=document.getElementById('masterPlay');
+let myProgressBar=document.getElementById('myProgressBar');
+let gif=document.getElementById('gif');
+let masterSongName=document.getElementById('masterSongName');
+let songItems=Array.from(document.getElementsByClassName('songItem'));
+
+let songs=[
+    {songName:"Ranjha-Shershah",filePath:"song_files/1.mp3",coverPath:"covers/1.jpg"},
+    {songName:"Oh Isha-Major",filePath:"song_files/2.mp3",coverPath:"covers/2.jpg"},
+    {songName:"Yoddha-Prithviraj",filePath:"song_files/3.mp3",coverPath:"covers/3.jpg"},
+    {songName:"Meri Jaan Meri Jaan-Bachchan Pandey",filePath:"song_files/4.mp3",coverPath:"covers/4.jpg"},
+    {songName:"Naacho Naacho-RRR",filePath:"song_files/5.mp3",coverPath:"covers/5.jpg"},
+    {songName:"Sholay-RRR",filePath:"song_files/6.mp3",coverPath:"covers/6.jpg"},
+    
+]
+
+songItems.forEach((element,i)=>{
+    element.getElementsByTagName("img")[0].src=songs[i].coverPath;
+    element.getElementsByClassName("songName")[0].innerText=songs[i].songName;
+})
+
+//audioElement.play();
+//handle play/pause click
+masterPlay.addEventListener('click',()=>{
+    if(audioElement.paused||audioElement.currentTime<=0){
+        audioElement.play();
+        masterPlay.classList.remove('fa-circle-play');
+        masterPlay.classList.add('fa-circle-pause');
+        gif.style.opacity=1;
+    }
+    else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-circle-pause');
+        masterPlay.classList.add('fa-circle-play');
+        gif.style.opacity=0;
+    }
+})
+//listen to events
+audioElement.addEventListener('timeupdate',()=>{
+    //seek bar update
+    progress=parseInt((audioElement.currentTime/audioElement.duration)*100);
+    myProgressBar.value=progress;
+})
+
+myProgressBar.addEventListener('change',()=>{
+    audioElement.currentTime=(myProgressBar.value*audioElement.duration/100);
+})
+
+const makeAllPlays=()=>{
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+    element.classList.remove('fa-circle-pause');
+    element.classList.add('fa-circle-play');
+    })
+}
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+    element.addEventListener('click',(e)=>{
+        makeAllPlays();
+        songIndex=parseInt(e.target.id);
+        e.target.classList.remove('fa-circle-play');
+        e.target.classList.add('fa-circle-pause');
+        audioElement.src = `song_files/${songIndex+1}.mp3`;
+        masterSongName.innerText = songs[songIndex].songName;
+        audioElement.currentTime=0;
+        audioElement.play();
+        gif.style.opacity=1;
+        masterPlay.classList.remove('fa-circle-play');
+        masterPlay.classList.add('fa-circle-pause');
+    })
+})
+
+document.getElementById('next').addEventListener('click',()=>{
+    if(songIndex>=5){
+        songIndex=0;
+    }
+    else{
+        songIndex +=1;
+    }
+    audioElement.src=`song_files/${songIndex+1}.mp3`;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime=0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-circle-play');
+    masterPlay.classList.add('fa-circle-pause');
+
+})
+
+document.getElementById('previous').addEventListener('click',()=>{
+    if(songIndex<=0){
+        songIndex=0;
+    }
+    else{
+        songIndex -=1;
+    }
+    audioElement.src=`song_files/${songIndex+1}.mp3`;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime=0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-circle-play');
+    masterPlay.classList.add('fa-circle-pause');
+    
+})
